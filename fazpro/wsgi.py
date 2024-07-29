d48145927bdc6e7ffb1f8c9fb949390091aa484d
Ali Raza <aliraza.zapta@gmail.com>
2024-07-29 11:21:20 +0500

    fix name issue

--- fazpro/wsgi.py
+++ fazpro/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for farzpro project.
+WSGI config for fazpro project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'farzpro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fazpro.settings')
 
 application = get_wsgi_application()
